26ac2897a57e9583f88a1cc60c2c8bad3ba02cdf
Xuan Vu <xuanbee@yahoo.com>
2019-10-29 01:40:33 -0700

    redesigned layout

--- react-metronome/src/Metronome.js
+++ react-metronome/src/Metronome.js
@@ -1,140 +1,185 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Metronome.css";
-import click1 from "./click1.wav";
-import click2 from "./click2.wav";
+import click1 from "./sound/click1.wav";
+import click2 from "./sound/click2.wav";
 
 class Metronome extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        // initializes states
-        this.state = {
-            playing: false,
-            count: 0,
-            bpm: 100,
-            input_bpm: this.bpm,
-            beatsPerMeasure: 4,
-            input_beats:  this.beatsPerMeasure
-        }
+    // initializes states
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 60,
+      beatsPerMeasure: 2,
+      stressFirstBeat: false,
+      time: 1,
+      timer: false
+    };
 
-        // creates Audio objects with webpack files
-        this.click1 = new Audio(click1);
-        this.click2 = new Audio(click2);
-    }
+    // creates Audio objects with webpack files
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
 
-    handleBpmChange = (new_bpm) => {
-        // sets bpm to input value
-        const bpm = new_bpm;
+  handleBpmChange = new_bpm => {
+    // sets bpm to input value
+    const bpm = new_bpm;
 
-        if (this.state.playing) {
-            // stop old timer and start a new one
-            clearInterval(this.timer);
-            this.timer = setInterval(
-                this.playClick, 
-                // 60 for per minute, 1000 to convert ms to s
-                (60 / bpm) * 1000
-            );
-            
-            // set new bpm and reset beat counter
-            this.setState({
-                count: 0,
-                bpm
-            });
-        }
-        // if not, just update bpm
-        else {
-            this.setState({bpm});
-        }
-    }
-    startStop = () => {
-        if (this.state.playing) {
-            // stop and clear the timer
-            clearInterval(this.timer);
-            this.setState({
-                playing: false
-            });
-        }
-        else {
-            // start a timer with the current bpm
-            this.timer = setInterval(
-                this.playClick, 
-                (60 / this.state.bpm) * 1000
-            );
-            this.setState(
-                {
-                    count: 0, 
-                    playing: true
-                }, 
-                // play a click immediately (after setState finishes)
-                this.playClick
-            );
-        }
-    }
-    playClick = () => {
-        const {count, beatsPerMeasure} = this.state;
+    if (this.state.playing) {
+      // stop old timer and start a new one
+      clearInterval(this.timer);
+      this.timer = setInterval(
+        this.playClick,
+        // 60 for per minute, 1000 to convert ms to s
+        (60 / bpm) * 1000
+      );
 
-        // first beat will have different sound than the others
-        if (count % beatsPerMeasure == 0) {
-            this.click2.play();
-        }
-        else {
-            this.click1.play();
-        }
+      // set new bpm and reset beat counter
+      this.setState({
+        count: 0,
+        bpm: bpm
+      });
+    }
+    // if not, just update bpm
+    else {
+      this.setState({ bpm: bpm });
+    }
+  };
+  startStop = () => {
+    if (this.state.playing) {
+      // stop and clear the timer
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      // start a timer with the current bpm
+      this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+      this.setState(
+        {
+          count: 0,
+          playing: true
+        },
+        // play a click immediately (after setState finishes)
+        this.playClick
+      );
+    }
+  };
+  playClick = () => {
+    const { count, beatsPerMeasure, stressFirstBeat } = this.state;
+    // first beat will have different sound than the others
+    if (stressFirstBeat && count === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+    // keep track of which beat we're on
+    this.setState({
+      count: (count + 1) % beatsPerMeasure
+    });
+  };
+  handleStressChange = () => {
+    this.setState({ stressFirstBeat: !this.state.stressFirstBeat });
+  };
+  handleTimeChange = () => {
+    this.setState({ timer: !this.state.timer });
+  };
+  handleSlider = event => {
+    this.handleBpmChange(parseInt(event.target.value));
+  };
+  minusBpm = () => {
+    if (this.state.bpm > 20) {
+      const new_bpm = this.state.bpm - 1;
+      this.handleBpmChange(new_bpm);
+    }
+  };
+  addBpm = () => {
+    if (this.state.bpm < 260) {
+      const new_bpm = this.state.bpm + 1;
+      this.handleBpmChange(new_bpm);
+    }
+  };
+  minusBeats = () => {
+    if (this.state.beatsPerMeasure > 2) {
+      this.setState({ beatsPerMeasure: this.state.beatsPerMeasure - 1 });
+    }
+  };
+  addBeats = () => {
+    if (this.state.beatsPerMeasure < 12) {
+      this.setState({ beatsPerMeasure: this.state.beatsPerMeasure + 1 });
+    }
+  };
+  createBeats = () => {
+    const { beatsPerMeasure, count } = this.state;
+    let beats = [];
+    for (var i = 0; i < beatsPerMeasure; i++) {
+      if ((i + 1) % beatsPerMeasure === count) {
+        beats.push(<i key={i} className="fas fa-circle"></i>);
+      } else {
+        beats.push(<i key={i} className="far fa-circle"></i>);
+      }
+    }
+    return beats;
+  };
 
-        // keep track of which beat we're on
-        this.setState(state => ({
-            count: (state.count + 1) % state.beatsPerMeasure
-        }));
-    }
-    handleChange_beats = (event) => {
-        this.input_beats = event.target.value;
-    }
-    handleSubmit_beats = (event) => {
-        event.preventDefault();
-        this.setState({
-            beatsPerMeasure: this.input_beats
-        });
-        // do you want to reset?
-        event.target.reset();
-    }
-    handleChange_bpm = (event) => {
-        this.input_bpm = event.target.value;
-    }
-    handleSubmit_bpm = (event) => {
-        event.preventDefault();
-        this.handleBpmChange(this.input_bpm);
-        // do you want to reset?
-        event.target.reset();
-    }
-    handleSlider = (event) => {
-        this.handleBpmChange(event.target.value);
-    }
+  render() {
+    // references variables from this.state
+    const { playing, bpm, beatsPerMeasure, time } = this.state;
 
-    render() {
-        // references variables from this.state
-        const {playing, bpm} = this.state;
-
-        return (
-            <div className="metronome">
-                <h1>React App Metronome</h1>
-                <div className="bpm-slider">
-                    <div>{bpm} BPM</div>
-                    <input type="range" min="60" max="240" value={bpm} onChange={this.handleSlider}/>
-                </div>
-                <form className="parameters" onSubmit={this.handleSubmit_bpm}>
-                    Enter BPM (60 to 240): 
-                    <input type="number" min="60" max="240" onChange={this.handleChange_bpm}/>
-                    <input type="submit" value="Submit"/>
-                </form>
-                <form className="parameters" onSubmit={this.handleSubmit_beats}>
-                    Enter number of beats: 
-                    <input type="number" onChange={this.handleChange_beats}/>
-                    <input type="submit" value="Submit"/>
-                </form>
-                <button className="startstop"onClick={this.startStop}>{playing ? "STOP" : "START"}</button>
-            </div>
-        );
-    }
+    return (
+      <div className="metronome">
+        <h1>React App Metronome</h1>
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <button onClick={this.minusBpm}>-</button>
+          <input
+            type="range"
+            min="20"
+            max="260"
+            value={bpm}
+            onChange={this.handleSlider}
+          />
+          <button onClick={this.addBpm}>+</button>
+        </div>
+        <div>{this.createBeats()}</div>
+        <button className="startstop" onClick={this.startStop}>
+          {playing ? "STOP" : "START"}
+        </button>
+        <div className="input">
+          <div>
+            <input
+              type="checkbox"
+              checked={this.state.stressFirstBeat}
+              onChange={this.handleStressChange}
+            />
+            Stress First Beat
+          </div>
+          <div className="beatsNum">
+            <button onClick={this.minusBeats}>-</button>
+            {beatsPerMeasure}
+            <button onClick={this.addBeats}>+</button>
+          </div>
+        </div>
+        <div className="input">
+          <div>
+            <input
+              type="checkbox"
+              checked={this.state.timer}
+              onChange={this.handleTimeChange}
+            />
+            Timer
+          </div>
+          <div className="beatsNum">
+            <button onClick={this.minusTime}>-</button>
+            {time}
+            <button onClick={this.addTime}>+</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Metronome;
--- react-metronome/src/index.js
+++ react-metronome/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Metronome from './Metronome';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Metronome from "./Metronome.js";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Metronome />, document.getElementById('root'));
+ReactDOM.render(<Metronome />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
